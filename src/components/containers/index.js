fcf17c16255723823ba60f71d85b0603ddd64056
Eli-b-good <eli.i.henderson95@gmail.com>
2020-06-21 14:24:14 -0400

    d

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,8 +1,9 @@
-export { default as SearchResultsContainer } from './SearchResultsContainer';
-export { Login, Signup } from './AuthContainer';
-export { default as HomePageContainer } from './HomePageContainer';
-export { default as UsersPageContainer } from './UsersPageContainer';
-export { default as AnimePageContainer } from './AnimePageContainer';
-export { default as SingleAnimePageContainer } from './SingleAnimePageContainer';
-export { default as CategoriesContainer } from './categoriesContainer';
-export { default as AnimeCategoriesPageContainer } from './AnimeCategoriesPageContainer';
+export { default as HomePageContainer } from "./HomePageContainer";
+export { default as UsersPageContainer } from "./UsersPageContainer";
+export { default as AnimePageContainer } from "./AnimePageContainer";
+export { default as SingleAnimePageContainer } from "./SingleAnimePageContainer";
+export { default as CategoriesContainer } from "./categoriesContainer";
+export { default as AnimeCategoriesPageContainer } from "./AnimeCategoriesPageContainer";
+export { default as SearchResultsContainer } from "./SearchResultsContainer";
+export { default as SearchResultsContainer } from "./SearchResultsContainer";
+export { Login, Signup } from "./AuthContainer";
