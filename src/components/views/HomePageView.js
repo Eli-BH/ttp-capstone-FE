4b3ea99c2b2c65723053fb754a5a851549f7bcbb
Eli-b-good <eli.i.henderson95@gmail.com>
2020-06-21 14:11:05 -0400

    file fixes

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -1,62 +1,60 @@
-import React, { useState } from 'react';
-import './styling.css';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import React, { useState } from "react";
+import "./HomePage.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 //import { Carousel } from "react-responsive-carousel";
-import AnimePageContainer from '../containers/AnimePageContainer';
-import { Carousel } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import CategoriesContainer from '../containers/categoriesContainer';
-import './styling.css';
-const HomePageView = props => {
-	const [index, setIndex] = useState(0);
+import AnimePageContainer from "../containers/AnimePageContainer";
+import { Carousel } from "react-bootstrap";
+import CategoriesContainer from "../containers/categoriesContainer";
+import { Link } from "react-router-dom";
 
-	const handleSelect = (selectedIndex, e) => {
-		setIndex(selectedIndex);
-	};
+const HomePageView = (props) => {
+  const [index, setIndex] = useState(0);
 
-	return (
-		<div>
-			<Carousel activeIndex={index} onSelect={handleSelect}>
-				{props.trendingAnime.map((
-					anime,
-					index, //add key //<a scr = "/:id"><img></a>
-				) => (
-					<Carousel.Item>
-						<img
-							src={anime.attributes.coverImage.large}
-							alt={anime.attributes.titles.en_jp}
-							className="trendingAnime"
-						></img>
-						<Carousel.Caption className="text-light">
-							<Link
-								to={`/${anime.id}`}
-								style={{ textDecoration: 'none', color: 'white' }}
-							>
-								<div className="carText">
-									<h4
-										className="legend"
-										onClick={() => props.handlePage(anime.id)}
-									>
-										{anime.attributes.titles.en_jp}
-									</h4>
-								</div>
-							</Link>
-						</Carousel.Caption>
-					</Carousel.Item>
-				))}
-			</Carousel>
-			<CategoriesContainer />
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
-			<div className="popAnimeTag">
-				<img
-					src="https://66.media.tumblr.com/efb3a6fc5a0e76829798bdb3d735ecf5/tumblr_mrnj0g6qza1rfjowdo1_500.gif"
-					alt="pikachu gif"
-				/>
-				<h1>Popluar Anime</h1>
-			</div>
-			<AnimePageContainer />
-		</div>
-	);
+  return (
+    <div>
+      {console.log("views ", typeof props.trendingAnime)}
+      <div>
+        <h1>Trending Anime</h1>
+      </div>
+
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        {props.trendingAnime.map((
+          anime,
+          index //add key //<a scr = "/:id"><img></a>
+        ) => (
+          <Carousel.Item>
+            <img
+              src={anime.attributes.coverImage.large}
+              alt={anime.attributes.titles.en_jp}
+              className="trendingAnime"
+            ></img>
+            <Carousel.Caption className=" text-light bg-dark">
+              <Link
+                to={`/${anime.id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <h5
+                  className="legend"
+                  onClick={() => props.handlePage(anime.id)}
+                >
+                  {anime.attributes.titles.en_jp}
+                </h5>
+              </Link>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <CategoriesContainer />
+      <div className="popAnimeTag">
+        <h1>Most Popular Anime</h1>
+      </div>
+      <AnimePageContainer />
+    </div>
+  );
 };
 
 // HomePageView.propTypes = {
