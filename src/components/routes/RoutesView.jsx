fdc3d3a6c7b95c745714dd577f24c7c0622ab8f5
Eli-b-good <eli.i.henderson95@gmail.com>
2020-06-21 14:24:53 -0400

    df+

--- src/components/routes/RoutesView.jsx
+++ src/components/routes/RoutesView.jsx
@@ -4,22 +4,57 @@ import {
   HomePageContainer,
   UsersPageContainer,
   SingleAnimePageContainer,
-  AnimeCategoriesPageContainer,
   SearchResultsContainer,
+  Login,
+  Signup,
+  AnimeCategoriesPageContainer,
 } from "../containers";
 
-const RoutesView = () => {
+const RoutesView = (props) => {
+  // const [cookies, setCookie] = useCookies(['name']);
+  // setCookie('name', 'bLAH', { path: '/' });
+  // const { isLoggedIn } = props;
+  // console.log(cookies);
+  // console.log(cookies.cookies);
+  // console.log('from routesview', cookies);
+
   return (
     <Switch>
       <Route exact path="/" component={HomePageContainer} />
-      <Route exact path="/:id" component={SingleAnimePageContainer} />
-      <Route exact path="/users/:id" component={UsersPageContainer} />
+      <Route exact path="/results/:term" component={SearchResultsContainer} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
       <Route
         exact
         path="/categories/:categories"
         component={AnimeCategoriesPageContainer}
       />
-      <Route exact paht="/results/:term" component={SearchResultsContainer} />
+      <Route exact path="/:id" component={SingleAnimePageContainer} />
+      {/* <Route exact path = "/logout" component={Logout}/> */}
+
+      <Route exact path="/users/:id" component={UsersPageContainer} />
+      {/* {isLoggedIn && (
+				<Switch>
+					{/* Routes placed within this section are only available after
+          logging in */}
+      {/* <Route
+            exact
+            path="/users/:id"
+            render={() => <UsersPageContainer cookies={props.cookies} />}
+          /> */}
+      {/* <Route
+						exact
+						path="/users/:id"
+						component={UsersPageContainer}
+						// cookies={props.cookies}
+					/>
+				</Switch>
+					)} */}
+      {/* Displays our Login component as a fallback */}
+
+      {/* <Route exact path="/users/:id" component={UsersPageContainer} /> */}
+
+      {/* <Route component={Login} /> */}
     </Switch>
   );
 };
