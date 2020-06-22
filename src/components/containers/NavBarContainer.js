af6dfec48b52eb20f7f04f32a7a136f55791b2be
Eli-b-good <eli.i.henderson95@gmail.com>
2020-06-22 19:44:46 -0400

    darkmode

--- src/components/containers/NavBarContainer.js
+++ src/components/containers/NavBarContainer.js
@@ -1,80 +1,80 @@
-import React, { Component } from 'react';
-import { searchAnimeThunk, logout } from '../../thunks';
-import NavBarView from '../views/NavBarView.js';
-import { connect } from 'react-redux';
-import { Redirect, withRouter } from 'react-router-dom';
-import {}
+import React, { Component } from "react";
+import { searchAnimeThunk, logout } from "../../thunks";
+import NavBarView from "../views/NavBarView.js";
+import { connect } from "react-redux";
+import { Redirect, withRouter } from "react-router-dom";
+
 class NavBarContainer extends Component {
-	constructor() {
-		super();
-		this.state = {
-			keyword: '',
-			returnResults: false,
-		};
-		this.handleChange = this.handleChange.bind(this);
-	}
+  constructor() {
+    super();
+    this.state = {
+      keyword: "",
+      returnResults: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-	componentDidMount() {
-		//this.props.searchAnime(this.state.keyword);
-	}
-	handleChange = event => {
-		this.setState({
-			keyword: event.target.value,
-		});
-	};
+  componentDidMount() {
+    //this.props.searchAnime(this.state.keyword);
+  }
+  handleChange = (event) => {
+    this.setState({
+      keyword: event.target.value,
+    });
+  };
 
-	handleSubmit = event => {
-		this.props.searchAnime(this.state.keyword);
-		this.setState({ returnResults: true });
-	};
+  handleSubmit = (event) => {
+    this.props.searchAnime(this.state.keyword);
+    this.setState({ returnResults: true });
+  };
 
-	handleLogout = () => {
-		this.props.logout();
-		this.props.history.push('/');
-	};
-	//
+  handleLogout = () => {
+    this.props.logout();
+    this.props.history.push("/");
+  };
+  //
 
-	render() {
-		const word = this.state.keyword;
+  render() {
+    const word = this.state.keyword;
 
-		if (this.state.returnResults === true) {
-			this.setState({ returnResults: false });
-			return (
-				<Redirect
-					to={{
-						pathname: `/results/${word}`,
-						search: '?utm=your+face',
-						state: { keyword: this.state.keyword },
-					}}
-				/>
-			);
-		}
+    if (this.state.returnResults === true) {
+      this.setState({ returnResults: false });
+      return (
+        <Redirect
+          to={{
+            pathname: `/results/${word}`,
+            search: "?utm=your+face",
+            state: { keyword: this.state.keyword },
+          }}
+        />
+      );
+    }
 
-		return (
-			<>
-				<NavBarView
-					handleChange={this.handleChange}
-					handleSubmit={this.handleSubmit}
-					user={this.props.user}
-					handleLogout={this.handleLogout}
-				/>
-			</>
-		);
-	}
+    return (
+      <>
+        <NavBarView
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          user={this.props.user}
+          handleLogout={this.handleLogout}
+        />
+      </>
+    );
+  }
 }
 
-const mapState = state => {
-	return {
-		results: state.search.data,
-		user: state.user,
-	};
+const mapState = (state) => {
+  return {
+    results: state.search.data,
+    user: state.user,
+  };
 };
 
-const mapDispatch = dispatch => {
-	return {
-		searchAnime: searchTerm => dispatch(searchAnimeThunk(searchTerm)),
-		logout: () => dispatch(logout()),
-	};
+const mapDispatch = (dispatch) => {
+  return {
+    searchAnime: (searchTerm) => dispatch(searchAnimeThunk(searchTerm)),
+    logout: () => dispatch(logout()),
+  };
 };
 
 export default withRouter(connect(mapState, mapDispatch)(NavBarContainer));
